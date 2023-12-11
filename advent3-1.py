9cbd053327aaf09e3c960924dc6f3b5400446fc6
Keidensu <sofdissem@gmail.com>
2023-12-11 18:32:52 -0500

    Day 3 part 1 complete, part 2 created but not done

---
+++ advent3-1.py
@@ -0,0 +1,36 @@
+import re
+
+def advent3():
+    f = open("input3.txt")
+    l=f.readlines()
+    l = [word.strip("\n") for word in l]
+    symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "/", "+", "="]
+    sum = 0
+
+
+
+    for i in range(len(l)):
+        nums = dict((m.start(), m.group()) for m in re.finditer(r'\d+', l[i]))
+        syms = dict((m.start(), m.group()) for m in re.finditer(r'[^a-zA-Z0-9\.]', l[i]))
+        symsdown = {}
+        symsup = {}
+
+        if i != len(l) - 1:
+            symsdown = dict((m.start(), m.group()) for m in re.finditer(r'[^a-zA-Z0-9\.]', l[i+1]))
+
+        if i != 0:
+            symsup = dict((m.start(), m.group()) for m in re.finditer(r'[^a-zA-Z0-9\.]', l[i-1]))
+        
+        for num in nums:
+            begin = num - 1
+            end = num + len(nums[num]) + 1
+            for k in range(begin,end):
+                if k in syms or k in symsdown or k in symsup:
+                    sum += int(nums[num])
+                    # print(nums[num])
+                    break
+
+    print(sum)
+
+if __name__ == "__main__":
+    advent3()
